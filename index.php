458f45bfd661e0cd6b63370c8cb7cb9a0f5f98cc
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-01-31 15:37:19 +0100

    Add reCaptcha

--- index.php
+++ index.php
@@ -4,27 +4,37 @@
     
     $validation_result;
     $query_status = null;
+    $reCaptcha_valid = false;
 
     if (!empty($_POST)){
-        $form = new Form(["firstname" => $_POST["firstname"], 
-                          "lastname" => $_POST["lastname"],
-                          "email" => $_POST["email"],
-                          "comment" => $_POST["comment"]
-                        ]);
+        $form = NULL;
 
-        $validation_result = $form->validate_form();
-        if (!is_array($validation_result)) {
-          $db = new Database();
-          $conn = $db->connect();
+        $secret_key = "6LeWXzskAAAAACDPnphSWG4_7CCEqjpActSt2zY7";
+        $reCaptcha_url = "https://www.google.com/recaptcha/api/siteverify?secret=".$secret_key."&response=".$_POST["g-recaptcha-response"];
+        $response = json_decode(file_get_contents($reCaptcha_url));
 
-          $insert_query = "
-                           INSERT INTO support_messages (firstname, lastname, email, comment)
-                           VALUES (:firstname, :lastname, :email, :comment)
-                          ";
-
-          $statement = $conn->prepare($insert_query);
-          $query_status = $statement->execute($form->get_fields());
-        }
+        $reCaptcha_valid = $response->success;
+        if ($reCaptcha_valid) {
+            $form = new Form(["firstname" => $_POST["firstname"], 
+                              "lastname" => $_POST["lastname"],
+                              "email" => $_POST["email"],
+                              "comment" => $_POST["comment"]
+                            ]);
+    
+            $validation_result = $form->validate_form();
+            if (!is_array($validation_result)) {
+              $db = new Database();
+              $conn = $db->connect();
+    
+              $insert_query = "
+                               INSERT INTO support_messages (firstname, lastname, email, comment)
+                               VALUES (:firstname, :lastname, :email, :comment)
+                              ";
+    
+              $statement = $conn->prepare($insert_query);
+              $query_status = $statement->execute($form->get_fields());
+            }
+        } 
     }
 ?>
 <!DOCTYPE html>
@@ -33,6 +43,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://www.google.com/recaptcha/api.js"></script>
         <title>Support | Hackers Poulette</title>
     </head>
     <body>
@@ -40,8 +51,12 @@
           if ($query_status){
             echo "<p style='color: green;'> Message sent successfully! </p>";
           }
+
+          if (!$reCaptcha_valid){
+            echo "<p style='color: red;'> Failed to submit form, please try again later...</p>";
+          }
         ?>
-        <form action="index.php" method="post">
+        <form id="support-form" action="index.php" method="post">
             <div>
                 <label for="firstname">First name</label>
                 <input type="text" name="firstname" id="firstname">
@@ -63,7 +78,19 @@
                 <?php echo isset($validation_result["comment"]) ? "<p style='color:red;'>$validation_result[comment]</p>" : "" ?>
             </div>
 
-            <input type="submit" name="submit" value="Send">
+            <input class="g-recaptcha" 
+                   data-sitekey="6LeWXzskAAAAAIIqj6d7jpf1hJ4_KAcLnqp-JvMa"
+                   data-callback="onSubmit"
+                   data-action="submit"
+                   type="submit"
+                   value="Send"
+            >
         </form>
+         <script>
+           function onSubmit(token) {
+             const form = document.querySelector("#support-form");
+             form.submit();
+          }
+         </script>
     </body>
 </html>
