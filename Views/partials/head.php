76c4c7b2e5efa2dde40f208a449fe1ab5d675b25
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-03-29 09:52:42 +0200

    Link css file

--- Views/partials/head.php
+++ Views/partials/head.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script src="https://cdn.tailwindcss.com"></script>
+        <link  rel="stylesheet" href="../../assets/main.css">
         <script src="https://www.google.com/recaptcha/api.js"></script>
         <title>Support | Hackers Poulette</title>
     </head>
